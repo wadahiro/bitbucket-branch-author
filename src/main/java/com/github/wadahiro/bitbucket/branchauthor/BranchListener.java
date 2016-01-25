d265e2da85218316bcd47cdfce915600fc6af07a
Hiroyuki Wada <wadahiro@gmail.com>
2016-01-25 13:32:12 +0900

    Support git push operation. Support branch deletion by merged pull request.

--- src/main/java/com/github/wadahiro/bitbucket/branchauthor/BranchListener.java
+++ src/main/java/com/github/wadahiro/bitbucket/branchauthor/BranchListener.java
@@ -1,15 +1,16 @@
 package com.github.wadahiro.bitbucket.branchauthor;
 
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.atlassian.activeobjects.external.ActiveObjects;
-import com.atlassian.bitbucket.event.branch.BranchCreatedEvent;
-import com.atlassian.bitbucket.event.branch.BranchDeletedEvent;
-import com.atlassian.bitbucket.repository.Branch;
+import com.atlassian.bitbucket.event.repository.AbstractRepositoryRefsChangedEvent;
+import com.atlassian.bitbucket.repository.RefChange;
+import com.atlassian.bitbucket.repository.RefChangeType;
 import com.atlassian.bitbucket.repository.Repository;
 import com.atlassian.bitbucket.user.ApplicationUser;
 import com.atlassian.event.api.EventListener;
@@ -27,21 +28,58 @@ public class BranchListener {
         this.activeObjects = activeObjects;
     }
 
+    /**
+     * We must use AbstractRepositoryRefsChangedEvent for handling all ADD/DELETE branch events.
+     * See https://jira.atlassian.com/browse/BSERV-4269.
+     * Using AbstractRepositoryRefsChangedEvent, we can support following operations.
+     * 
+     * Branch creation from GUI
+     * Branch deletion from GUI
+     * Branch creation from Git client (git push origin <branch_name>)
+     * Branch deletion from Git client (git push --delete origin <branch_name>)
+     * Branch deletion by merged pull request(use source branch deletion)
+     * Branch creation by fork syncing
+     * Branch deletion by fork syncing
+     * 
+     * @param event
+     */
     @EventListener
-    public void onBranchCreated(BranchCreatedEvent event) {
+    public void onRefsChanged(AbstractRepositoryRefsChangedEvent event) {
         Repository repo = event.getRepository();
         ApplicationUser user = event.getUser();
-        Branch branch = event.getBranch();
 
         Integer repoId = repo.getId();
-        Date created = event.getDate();
-        String branchRef = branch.getId();
         Integer userId = user.getId();
         String userEmail = user.getEmailAddress();
 
-        log.info("Save branch author. repoId={} created={}, branchRef={}, userId={}, userEmail={}", repoId, created,
-                branchRef, userId, userEmail);
+        Collection<RefChange> refChanges = event.getRefChanges();
 
+        for (RefChange refChange : refChanges) {
+            RefChangeType type = refChange.getType();
+
+            if (type == RefChangeType.ADD) {
+                Date created = event.getDate();
+                String branchRef = refChange.getRef().getId();
+                event.getUser();
+
+                log.info(
+                        "AbstractRepositoryRefsChangedEvent: Save branch author. repoId={} created={}, branchRef={}, userId={}, userEmail={}",
+                        repoId, created, branchRef, userId, userEmail);
+
+                createBranchAuthor(repoId, created, branchRef, userId, userEmail);
+
+            } else if (type == RefChangeType.DELETE) {
+                String branchRef = refChange.getRef().getId();
+
+                log.info("AbstractRepositoryRefsChangedEvent: Delete branch author. repoId={}, branchRef={}", repoId,
+                        branchRef);
+
+                deleteBranchAuthor(repoId, branchRef);
+            }
+        }
+    }
+
+    private void createBranchAuthor(Integer repoId, Date created, String branchRef, Integer userId, String userEmail) {
         try {
             BranchAuthorImpl.saveBranchAuthor(activeObjects, repoId, created, branchRef, userId, userEmail);
         } catch (SQLException e) {
@@ -49,16 +87,7 @@ public class BranchListener {
         }
     }
 
-    @EventListener
-    public void onBranchDeleted(BranchDeletedEvent event) {
-        Repository repo = event.getRepository();
-        Branch branch = event.getBranch();
-
-        Integer repoId = repo.getId();
-        String branchRef = branch.getId();
-
-        log.info("Delete branch author. repoId={}, branchRef={}", repoId, branchRef);
-
+    private void deleteBranchAuthor(Integer repoId, String branchRef) {
         try {
             BranchAuthorImpl.deleteBranchAuthor(activeObjects, repoId, branchRef);
         } catch (SQLException e) {
